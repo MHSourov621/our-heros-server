9b63282a6b0c0bda42f297bc529377dfc3033c4a
MHSourov621 <mhsourov621@gmail.com>
2023-05-20 03:31:07 +0600

    delete method added

--- index.js
+++ index.js
@@ -96,6 +96,13 @@ async function run() {
             console.log(result);
         })
 
+        app.delete('/products/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)};
+            const result = await productCollection.deleteOne(query);
+            res.send(result)
+        })
+
 
 
         // Send a ping to confirm a successful connection
