a4a9314566939e96a45a96df5b2b7fca6206db71
MHSourov621 <mhsourov621@gmail.com>
2023-05-20 01:24:10 +0600

    get data by email

--- index.js
+++ index.js
@@ -59,6 +59,11 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/email/:email', async(req, res) => {
+            const result = await productCollection.find({email: req.params.email}).toArray();
+            res.send(result)
+        })
+
         app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
