a56e0036b14ccb4ce4bfd3e1d70c06aac6c272f0
MHSourov621 <mhsourov621@gmail.com>
2023-05-19 17:21:18 +0600

    find by category

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -32,9 +32,21 @@ async function run() {
 
 
         app.get('/products', async(req, res) => {
-            const cursor = productCollection.find();
+            const cursor = productCollection.find().limit(20);
             const result = await cursor.toArray();
             res.send(result)
+        })
+
+        app.get('/products/:category', async(req, res) => {
+            const result = await productCollection.find({ category: req.params.category }).toArray();
+            res.send(result)
+        })
+
+        app.get('/products/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id : new ObjectId(id)};;
+            const result = await productCollection.findOne(query);
+            res.send(result);
         })
 
         app.post('/products', async(req, res) => {
